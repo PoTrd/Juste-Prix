8749530cb38098a36a44214ead07d4ac12305955
PoTrd <94467775+PoTrd@users.noreply.github.com>
2022-08-16 15:01:37 +0200

    Update juste_prix.py
    Ajout d'une fonctionnalité de calcul du nombre de tentatives avec une simple variable rajoutée dans la boucle while.

--- juste_prix.py
+++ juste_prix.py
@@ -2,13 +2,16 @@ import random
 
 n = random.randint(1, 10)
 user_number = 0
+tentatives = 0
 
 while user_number != n:
 
     user_number = int(input("Choisis un nombre entre 1 et 10 : "))
+    tentatives += 1
 
     if user_number == n:
-        print("Bravo, tu as gagnés le Juste Prix !")
+        print(
+            "Bravo, tu as gagnés le Juste Prix avec {0} tentatives !" .format(tentatives))
     elif user_number < n and user_number >= 1:
         print("C'est plus !")
     elif user_number > n and user_number <= 10:
